f52431e7c26ce4dd58bc5fd461ca8f4e95d22119
Zipper213 <varholikb@yahoo.com>
2018-08-22 17:10:37 -0500

    Update StorageManager.java

--- src/storage/StorageManager.java
+++ src/storage/StorageManager.java
@@ -1,1 +1,132 @@
+public class StorageManager {
+    public static String S1, S2, S3, S4, S5;
+    public static int I1, I2, I3, I4, I5;
+    public static boolean B1, B2, B3, B4, B5;
+    
+    public static void setString1(String value) {
+        S1 = value;
+    }
+    
+    public static void setString2(String value) {
+        S2 = value;
+    }
+    
+    public static void setString3(String value) {
+        S3 = value;
+    }
+    
+    public static void setString4(String value) {
+        S4 = value;
+    }
+    
+    public static void setString5(String value) {
+        S5 = value;
+    }
+    
+    
+    public static String getString1() {
+        return S1;
+    }
+    
+    public static String getString2() {
+        return S2;
+    }
+    
+    public static String getString3() {
+        return S3;
+    }
+    
+    public static String getString4() {
+        return S4;
+    }
+    
+    public static String getString5() {
+        return S5;
+    }
+    
+    ////////////////////////////////////INT
+    
+    public static void setInt1(int value) {
+        I1 = value;
+    }
+    
+    public static void setInt2(int value) {
+        I2 = value;
+    }
+    
+    public static void setInt3(int value) {
+        I3 = value;
+    }
+    
+    public static void setInt4(int value) {
+        I4 = value;
+    }
+    
+    public static void setInt5(int value) {
+        I5 = value;
+    }
+    
+    
+    public static int getInt1() {
+        return I1;
+    }
+    
+    public static int getInt2() {
+        return I2;
+    }
+    
+    public static int getInt3() {
+        return I3;
+    }
+    
+    public static int getInt4() {
+        return I4;
+    }
+    
+    public static int getInt5() {
+        return I5;
+    }
+    
+    /////////////////////////////////BOOLEAN
+    
+    public static void setBool1(boolean value) {
+        B1 = value;
+    }
+    
+    public static void setBool2(boolean value) {
+        B2 = value;
+    }
+    
+    public static void setBool3(boolean value) {
+        B3 = value;
+    }
+    
+    public static void setBool4(boolean value) {
+        B4 = value;
+    }
+    
+    public static void setBool5(boolean value) {
+        B5 = value;
+    }
+    
+    
+    public static void getBool1() {
+        return B1;
+    }
 
+    public static void getBool2() {
+        return B2;
+    }
+
+    public static void getBool3() {
+        return B3;
+    }
+
+    public static void getBool4() {
+        return B4;
+    }
+
+    public static void getBool5() {
+        return B5;
+    }
+    
