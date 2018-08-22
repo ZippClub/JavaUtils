e27fecb393549ca055577dc2ce46b21662bfcb65
Zipper213 <varholikb@yahoo.com>
2018-08-22 16:49:49 -0500

    Create MathUtils.java

---
+++ src/math/MathUtils.java
@@ -0,0 +1,13 @@
+public class MathUtils {
+    
+    public static int add(int x, int y) {return x+y;}
+    public static int sub(int x, int y) {return x-y;}
+    public static int multi(int x, int y) {return x*y;}
+    public static int div(int x, int y) {return x/y;)
+    
+    //add(2, 2) return 4
+    //sub(4. 2) return 2
+    //multi(3, 4) return 12
+    //div(6, 2) return 3;
+    
+}
